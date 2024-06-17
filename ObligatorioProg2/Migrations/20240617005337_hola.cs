371cde1ec7fb7b87f3806e96f1e6354171eea7da
sebarambi <sebarambi@yahoo.com>
2024-06-16 21:55:59 -0300

    Volver a empezar

---
+++ ObligatorioProg2/Migrations/20240617005337_hola.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ObligatorioProg3.Migrations
+{
+    /// <inheritdoc />
+    public partial class hola : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Socio_Local_LocalId",
+                table: "Socio");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "LocalId",
+                table: "Socio",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Socio_Local_LocalId",
+                table: "Socio",
+                column: "LocalId",
+                principalTable: "Local",
+                principalColumn: "IdLocal",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Socio_Local_LocalId",
+                table: "Socio");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "LocalId",
+                table: "Socio",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Socio_Local_LocalId",
+                table: "Socio",
+                column: "LocalId",
+                principalTable: "Local",
+                principalColumn: "IdLocal");
+        }
+    }
+}
